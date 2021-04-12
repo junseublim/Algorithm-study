ec7933f54fdb65866f1bb7b0fc7410dfc7354dfc
2016026480 <junslim11@gmail.com>
2021-04-12 23:42:29 +0900

    class 4

--- baekjoon/1865.cpp
+++ baekjoon/1865.cpp
@@ -3,31 +3,7 @@
 using namespace std;
 typedef pair<int,int> pii;
 vector<pii> edges[501];
-vector<pii> hole[501];
-vector<pii> hse;
-int holeTime[200];
-
-int go(int s, int e) {
-    queue<pii> q;
-    q.push(make_pair(s,0));
-    int visited[501];
-    for (int i =0; i<501; i++ ) {
-        visited[i] = 987654321;
-    }
-    while(!q.empty()) {
-        pii nxt = q.front();
-        q.pop();
-        if (visited[nxt.first] <= nxt.second) continue;
-        visited[nxt.first] = nxt.second;
-        for (int i =0; i<edges[nxt.first].size(); i++) {
-            q.push(make_pair(edges[nxt.first][i].first , nxt.second + edges[nxt.first][i].second ));
-        }
-        for (int i =0; i<hole[nxt.first].size(); i++) {
-            q.push(make_pair(edges[nxt.first][i].first , nxt.second - edges[nxt.first][i].second ));
-        }
-    }
-    return visited[e];
-}
+int dist[501];
 
 
 int main() {
@@ -38,9 +14,8 @@ int main() {
         cin>>n>>m>>w;
         for (int i =0; i<501; i++) {
             edges[i].clear();
-            hole[i].clear();
+            dist[i] = 987654321;
         }
-        hse.clear();
         for (int i =0; i<m; i++) {
             int s,e,t;
             cin>>s>>e>>t;
@@ -50,14 +25,32 @@ int main() {
         for (int i =0; i<w; i++) {
             int s,e,t;
             cin>>s>>e>>t;
-            hole[s].push_back(make_pair(e,t));
-            hse.push_back(make_pair(s,e));
-            holeTime[i] = t;
+            edges[s].push_back(make_pair(e,-1*t));
         }
         int found = 0;
-        for (int i =0; i<hse.size(); i++) {
-            int dist = go(hse[i].second, hse[i].first);
-            if (dist < holeTime[i]) found = 1;
+        for (int l = 1; l <= n; l++) {
+            if (dist[l] == 987654321) dist[l] = 0;
+            else continue;
+            for (int i = 1; i<=n; i++) {
+                for (int j =1; j<=n; j++) {
+                    for (int k =0; k<edges[j].size(); k++) {
+                        int d = edges[j][k].first;
+                        int cost = edges[j][k].second;
+                        dist[d] = min(dist[d], dist[j]+cost);
+                    }
+                }
+            }
+        }
+        for (int i =1; i<=n; i++) {
+            for (int j = 0; j<edges[i].size(); j++) {
+                int d = edges[i][j].first;
+                int cost = edges[i][j].second;
+                if (dist[d] > cost + dist[i]) {
+                    found = 1;
+                    break;
+                }
+            }
+            if (found)break;
         }
         if (found) {
             cout<<"YES"<<endl;
---
+++ baekjoon/2206.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef pair<int,int> pii;
+int visited[1000][1000];
+string vs[1000];
+int my[] = {0,0,-1,1};
+int mx[] = {-1,1,0,0};
+
+typedef struct Node {
+     int y;
+     int x;
+     int t;
+     int broke;
+    Node( int yy,  int xx,  int tt,  int b) {
+        y=yy;x=xx;t=tt;
+        broke = b;
+    }
+} Node;
+int n,m;
+
+int bfs(int y, int x) {
+    queue<Node> q;
+    q.push(Node(y,x,1,0));
+    while(!q.empty()) {
+        Node nxt = q.front();
+        q.pop();
+        if (visited[nxt.y][nxt.x] == 1) continue;
+        else if (visited[nxt.y][nxt.x] == 2 && nxt.broke == 1) {
+            continue;
+        } 
+        if (vs[nxt.y][nxt.x] == '1')
+        {
+            if (nxt.broke == 0) {
+                nxt.broke = 1;
+            }
+            else continue;
+        }
+        else visited[nxt.y][nxt.x] = nxt.broke+1;
+        if (nxt.y == n-1 && nxt.x == m-1) {
+            return nxt.t;
+        }
+        for (int i =0; i<4; i++) {
+            if (nxt.y+my[i] >= 0 && nxt.y+my[i] < n && nxt.x+mx[i] >= 0 && nxt.x+mx[i] < m) {
+                q.push(Node(nxt.y+my[i], nxt.x+mx[i], nxt.t+1, nxt.broke));
+            }
+        }
+    }
+    return -1;
+}
+
+
+int main() {
+    
+    cin>>n>>m;
+    
+    for (int i = 0; i<n; i++) {
+        string s = "";
+        cin>>s;
+        vs[i] = s;
+    }
+    memset(visited,0, sizeof(visited));
+    cout<<bfs(0,0);
+}
